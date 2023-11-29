68bebf98f56959f91bf5645bd4086a4cedfda656
JmoanMoura <joanmoura2014@gmail.com>
2023-11-29 01:46:06 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("ligth")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("ligth")) {
+    // se tiver com ligth mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    // se tiver sem ligth mode, adicionar a imagem normal
+    img.setAttribute("src", "./assets/avatar1.png")
+  }
+}
